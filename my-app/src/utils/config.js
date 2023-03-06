e621b4bb68bb6e4cdc5aee6ed825dd37361fbd61
Guillaume Darroux <g.darroux@gmail.com>
2023-03-06 19:10:54 +0100

    API_URL digitalocean config

--- my-app/src/utils/config.js
+++ my-app/src/utils/config.js
@@ -1,3 +1,3 @@
 //export const API_URL = "http://localhost:1337";
 
-export const API_URL = "app-2e726350-5ea2-4d5f-b726-82bd9dacd36d"; // config digitalocean
+export const API_URL = "https://celestine-naturo-strapi-do-u7hts.ondigitalocean.app"; // config digitalocean
